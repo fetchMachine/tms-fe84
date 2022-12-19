885fe6b6718f4370faa5a34e0b5734a6aa9602eb
Roman Makretski <32076718+fetchMachine@users.noreply.github.com>
2022-12-19 21:07:53 +0300

    Update 02-ts_extended.ts

--- hw/02-ts_extended.ts
+++ hw/02-ts_extended.ts
@@ -1,27 +1,28 @@
-class BaseUser {
+
+// создать с помощью Record тип объекта ключи которого любая строка, а значения строка или число
+type myCollectionOfNumberOrString = any;
+
+interface User {
+  id: number;
   name: string;
+  age: number;
+  friends: User[];
 }
 
-// создать два новых классов Admin и Guest, которые должные быть наследниками класс BaseUser
+// создать интерфейс на основе User у которого нет поля id, а все остальные поля - опциональные
+type NewUser = any;
 
+// Написать дженерик тип, который достает второй параметр функии
+type Sum = (a: number, b: number) => number;
+type Log = (msg: string, role: 'admin' | 'user') => number;
 
-// типизровать функцию asArray у которой взаимосвязаны типы входящего параметра и возврааемого значения
-const asArray = (value) => [ value ];
+type SecondParam = any;
+// напр: SecondParam<typeof Sum> => number
+// напр: SecondParam<typeof Log> => 'admin' | 'user'
 
+// сделать тип обязательным с помощью утили тайпов, т.е. чтобы не мог быть null
+type Type = string | number | boolean | null | undefined;
+type TypeWithNull = any;
 
-interface Car {
-  id:
-}
-
-getByKey
-
-Partial
-Required
-Pick
-  Picl + intersection
-Omit
-Exlude
-Extract
-ReturnType / Parameters
-NonNullable
-  Сделать свой nullable
+// написать дженерик обратный NonNullable, т.е. чтобы к текущему типу добавлся тип null | undefined;
+type Nullable = any;
