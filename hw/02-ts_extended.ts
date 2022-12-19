43e63b7f0ca970a1bd32c6848def1d4d37360bf3
r.makretski <r.makretski@andersenlab.com>
2022-12-19 18:13:04 +0300

    add: add ts-extended template

---
+++ hw/02-ts_extended.ts
@@ -0,0 +1,27 @@
+class BaseUser {
+  name: string;
+}
+
+// создать два новых классов Admin и Guest, которые должные быть наследниками класс BaseUser
+
+
+// типизровать функцию asArray у которой взаимосвязаны типы входящего параметра и возврааемого значения
+const asArray = (value) => [ value ];
+
+
+interface Car {
+  id:
+}
+
+getByKey
+
+Partial
+Required
+Pick
+  Picl + intersection
+Omit
+Exlude
+Extract
+ReturnType / Parameters
+NonNullable
+  Сделать свой nullable
